e845f879b9ce92bb5738dd1928dac3513942dcca
Sebastian <sl5softwarelab@gmail.com>
2018-12-01 11:06:56 +0100

    menu always open`, experimental feature
    Menu, Tray, add, set g_min_searchWord_length := 0 (it stays open`, experimental feature), lbl_g_min_searchWord_length_0
    	Menu, Tray, add, set g_min_searchWord_length := 1, lbl_g_min_searchWord_length_1

--- Source/sql/template/select01.sql
+++ Source/sql/template/select01.sql
@@ -1,6 +1,7 @@
-SELECT word, worddescription, wordreplacement
+SELECT distinct word, worddescription, wordreplacement
      FROM Words
      WHERE wordindexed GLOB upper('s') || '*'
      and   ActionListID > 0 order by ROWID desc
      LIMIT 9
 -- you need to reload the script after each change. be careful by changing the ware statment. its will later parsed by script.
+-- prp probab ür probab pro proba qahk s changin pro probab pro probab p proba
